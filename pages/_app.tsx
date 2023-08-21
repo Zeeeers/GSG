12f1426802b0696e13898401883db00c5179d6e0
Nicolás Henríquez <nhenriquez619@gmail.com>
2023-08-21 11:39:12 -0400

    feat(global): implement tag manage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,19 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { appWithTranslation } from 'next-i18next';
 import { ChakraProvider } from '@chakra-ui/react';
+import TagManager from 'react-gtm-module';
 
 // Styles
 import theme from 'styles/theme';
+import { useEffect } from 'react';
 
 // CustomApp
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
     // States
+
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-TVKW5HK5' });
+    }, []);
 
     return (
         <>
